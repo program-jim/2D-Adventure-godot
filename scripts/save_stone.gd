bcba5973b96d9d5b3c9f56173eaa809bacaee2f5
Program-Jim <1072118335@qq.com>
2025-07-30 13:21:22 +0800

    Add external fonts.
    Add smiley-sans and Roboto.

--- scripts/save_stone.gd
+++ scripts/save_stone.gd
@@ -1,3 +1,13 @@
+####################
+#
+#	File Name: save_stone.gd
+#	File Created Date: July 29 2025
+#	Language: GDScript
+#	File Description:
+#		Save stone behavior.
+#
+####################
+
 extends Interactable
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
