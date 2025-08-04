592a44a52012ec8d216953905dd437c65e998bed
Program-Jim <1072118335@qq.com>
2025-08-04 11:43:22 +0800

    Update sound_manager.gd

--- scripts/sound_manager.gd
+++ scripts/sound_manager.gd
@@ -15,24 +15,24 @@ func play_sfx(name: String) -> void:
 		return
 	else:
 		player.play()
-		
-		
+
+
 func play_bgm(stream: AudioStream) -> void:
 	if not is_all_bgm_play:
 		return
 	if bgm_player.stream == stream and bgm_player.playing:
 		return
 	bgm_player.stream = stream
-	bgm_player.play()	
-		
+	bgm_player.play()
+
 
 func setup_ui_sounds(node: Node) -> void:
 	var button := node as Button
 	if button:
 		button.pressed.connect(play_sfx.bind("UIPress"))
 		button.focus_entered.connect(play_sfx.bind("UIFocus"))
-		
-		
+
+
 func setup_ui_sounds_with_all_children(node: Node) -> void:
 	setup_ui_sounds(node)
 	
